2c6cc17ac0d37229f71101fb1dfb3390bd432705
Pamuditha Rajapaksha <pamuditharajapaksha@gmail.com>
2022-09-30 21:18:41 +0530

    gtm initialize

--- src/services/api.js
+++ src/services/api.js
@@ -3,5 +3,5 @@ const PRODUCTION_API =
   process.env.API || "https://ecommerce-app-back.herokuapp.com/api";
 const DEVELOPMENT_API = `http://localhost:${PORT}/api`;
 
-export const api =
-  process.env.NODE_ENV === "development" ? DEVELOPMENT_API : PRODUCTION_API;
+export const api = PRODUCTION_API;
+// process.env.NODE_ENV === "development" ? DEVELOPMENT_API : PRODUCTION_API;
