001830ab70fe18e1ee6e6f267c617a7d141ba167
Pamuditha Rajapaksha <pamuditharajapaksha@gmail.com>
2022-10-03 21:34:33 +0530

    btn

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import Main from "./Main";
 import "react-toastify/dist/ReactToastify.css";
+import "./css/accept.css";
 
 function App() {
   return (
     <>
       <Main />
+      <button
+        id="accept-btn"
+        className="accept-btn"
+        name="accept-btn"
+        style={{ position: "fixed", bottom: 50, right: 50 }}
+      >
+        Accept
+      </button>
     </>
   );
 }
