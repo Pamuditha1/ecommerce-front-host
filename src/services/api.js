5e6a33e941f9a4f306b051ae32a57a1ce089004d
Pamuditha Rajapaksha <pamuditharajapaksha@gmail.com>
2023-04-18 16:53:22 +0530

    api updated

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 const PORT = 3002;
-const PRODUCTION_API = process.env.REACT_APP_API
+const PRODUCTION_API = process.env.REACT_APP_API || "https://dsccapp.azurewebsites.net/api"
 const DEVELOPMENT_API = `http://localhost:${PORT}/api`;
 
-export const api =
-  process.env.NODE_ENV === "development" ? DEVELOPMENT_API : PRODUCTION_API;
+export const api = PRODUCTION_API
+  // process.env.NODE_ENV === "development" ? DEVELOPMENT_API : PRODUCTION_API;
